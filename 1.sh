c7e90c32fa85f7a6468f250594d2ad20e7b16ccc
piscox <64281992+piscox@users.noreply.github.com>
2020-04-28 06:59:35 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -208,10 +208,10 @@ rm -f ssl.sh
 
 # install webmin
 cd
-wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.900_all.deb"
-dpkg --install webmin_1.900_all.deb;
+wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.941_all.deb"
+dpkg --install webmin_1.941_all.deb;
 apt-get -y -f install;
-rm /root/webmin_1.900_all.deb
+rm /root/webmin_1.941_all.deb
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
 
