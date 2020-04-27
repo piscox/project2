3464d67e0cbd9a66de030b090c106ffdc2fc913b
piscox <64281992+piscox@users.noreply.github.com>
2020-04-28 03:32:42 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
-nano /etc/systemd/system/rc-local.service
-chmod +x /etc/rc.local
-systemctl enable rc-local
-systemctl start rc-local.service
+# rc.local
+wget -O /etc/rc.local "$source/rc.local";chmod +x /etc/rc.local
+wget -O /etc/iptables.up.rules "$source/iptables.up.rules"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+MYIP=`curl -s ifconfig.me`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
